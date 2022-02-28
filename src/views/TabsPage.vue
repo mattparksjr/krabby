3e16bdccba6f4f0e7bcf5ddf680209f6629cd13f
Matthew Parks <mattparksjr@gmail.com>
2022-02-28 11:50:45 -0500

    Stuff

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,22 +4,22 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
+          <ion-icon :icon="homeOutline" />
           <ion-label>Home</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
+          <ion-icon :icon="cashOutline" />
           <ion-label>Income</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="walletOutline" />
           <ion-label>Spending</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab4" href="/tabs/tab4">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="personCircleOutline" />
           <ion-label>Account</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -30,16 +30,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { walletOutline, cashOutline, homeOutline, personCircleOutline } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      walletOutline, 
+      cashOutline, 
+      homeOutline,
+      personCircleOutline
     }
   }
 });
